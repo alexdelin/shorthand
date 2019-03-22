aa4ddda7bbd2816429ba58355c759e6214a7b20e
Alex Delin <alexdelin@me.com>
2019-03-21 22:50:13 -0400

    add barebones cli tool

--- notes/note_parser/stamp_notes.py
+++
@@ -1,9 +0,0 @@
-import re
-from datetime import datetime
-from subprocess import Popen, PIPE
-from note_parser.utils.patterns import CATCH_ALL_PATTERN, \
-        VALID_INCOMPLETE_PATTERN, VALID_COMPLETE_PATTERN, \
-        UNFINISHED_UNSTAMPED_PATTERN, FINISHED_START_STAMPED_PATTERN, \
-        FINISHED_UNSTAMPED_PATTERN
-
-
