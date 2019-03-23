b82f55092bf6f9ee5716c3bad46a7b6ffd9460f6
Alex Delin <alexdelin@me.com>
2019-03-23 14:59:07 -0400

    implement and wire up endpoint to show context

--- notes/web/api.py
+++ notes/web/api.py
@@ -9,7 +9,7 @@ import json
 from flask import Flask, request  # , render_template, send_from_directory
 
 from note_parser.todo_tools import get_todos  # , mark_todo
-from note_parser.search_tools import search_notes
+from note_parser.search_tools import search_notes, get_context
 from note_parser.utils.config import get_notes_config
 
 app = Flask(__name__)
@@ -36,6 +36,16 @@ def get_search_results():
                 notes_directory=NOTES_CONFIG['notes_directory'],
                 query_string=query_string,
                 case_sensitive=case_sensitive))
+
+
+@app.route('/get_context', methods=['GET'])
+def get_line_context():
+
+    filename = request.args.get('filename')
+    line_number = int(request.args.get('line_number'))
+    width = int(request.args.get('width', 5))
+
+    return json.dumps(get_context(filename, line_number, width))
 
 
 if __name__ == "__main__":
