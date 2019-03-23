f0f38a50c4d810d43e0708cec75d63e20e9a249f
Alex Delin <alexdelin@me.com>
2019-03-23 14:20:40 -0400

    wire up api endpoint for notes search

--- notes/web/api.py
+++ notes/web/api.py
@@ -6,9 +6,10 @@ Note Parser Web API
 
 import json
 
-from flask import Flask  # , request, render_template, send_from_directory
+from flask import Flask, request  # , render_template, send_from_directory
 
 from note_parser.todo_tools import get_todos  # , mark_todo
+from note_parser.search_tools import search_notes
 from note_parser.utils.config import get_notes_config
 
 app = Flask(__name__)
@@ -23,6 +24,18 @@ def get_current_todos(path):
     return json.dumps(get_todos(
                 notes_directory=NOTES_CONFIG['notes_directory'],
                 todo_status=path))
+
+
+@app.route('/search', methods=['GET'])
+def get_search_results():
+
+    query_string = request.args.get('query_string')
+    case_sensitive = request.args.get('case_sensitive')
+
+    return json.dumps(search_notes(
+                notes_directory=NOTES_CONFIG['notes_directory'],
+                query_string=query_string,
+                case_sensitive=case_sensitive))
 
 
 if __name__ == "__main__":
