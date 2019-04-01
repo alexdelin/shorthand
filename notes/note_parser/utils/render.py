a8fa6dfc0bda319973d4a46b48e549ca01205ba1
Alex Delin <alexdelin@me.com>
2019-03-31 21:53:50 -0400

    read notes files as utf-8

--- notes/note_parser/utils/render.py
+++ notes/note_parser/utils/render.py
@@ -1,9 +1,11 @@
+import codecs
+
 import markdown
 
 
 def get_rendered_markdown(file_path):
 
-    with open(file_path, 'r') as input_file_object:
+    with codecs.open(file_path, mode="r", encoding="utf-8") as input_file_object:
         markdown_content = input_file_object.read()
 
     html_content = markdown.markdown(markdown_content, extensions=['fenced_code'])
