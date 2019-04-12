11effffca2553996a67a286e54db81b411addcc8
Alex Delin <alexdelin@me.com>
2019-04-12 16:10:56 -0400

    remove old implementations

--- notes/web/js/notes.js
+++ notes/web/js/notes.js
@@ -86,47 +86,6 @@ function setResultActions(resultType) {
     }
 }
 
-/*
-function fetchTodos(status) {
-    console.log( "Handler for refresh content called." );
-    $('#searchContent')[0].style.display = 'none'
-    $('#todoContent')[0].style.display = 'block'
-    $('#questionContent')[0].style.display = 'none'
-
-    $('#todoList')[0].innerHTML = ''
-    var directoryFilter = $('#directoryFilter')[0].value
-    console.log(directoryFilter)
-    $.get('get_todos', {status: status, directory_filter: directoryFilter},
-        function(todoData){
-            var loadedData = JSON.parse(todoData)
-            var todoListElement = ''
-            _.each(loadedData, function(todoResult) {
-                var text = todoResult['todo_text']
-                var file = todoResult['file_path']
-                var startDate = todoResult['start_date']
-                var endDate = todoResult['end_date']
-                var line = todoResult['line_number']
-                var newRowElement = getTodoElement(text, file, startDate, endDate, line)
-                todoListElement = todoListElement + newRowElement
-            })
-            $('#todoList').append(todoListElement)
-            setResultActions('todo')
-    });
-}
-
-$("#fetchIncomplete").click(function() {
-    fetchTodos('incomplete')
-});
-
-$("#fetchComplete").click(function() {
-    fetchTodos('complete')
-});
-
-$("#fetchSkipped").click(function() {
-    fetchTodos('skipped')
-});
-*/
-
 function renderTodoResults(todoData) {
     // Set visibility of results containers
     $('#todoList')[0].innerHTML = ''
@@ -158,39 +117,6 @@ $("#stampNotes").click(function() {
     });
 });
 
-/*
-$("#searchSubmit").click(function() {
-
-    console.log( "Handler for search submit called." );
-    $('#searchContent')[0].style.display = 'block'
-    $('#todoContent')[0].style.display = 'none'
-    $('#questionContent')[0].style.display = 'none'
-
-    $('#searchResults')[0].innerHTML = ''
-    var queryString = $('#searchQuery')[0].value
-
-    $.get('search', {query_string: queryString},
-        function(searchData){
-            var loadedData = JSON.parse(searchData)
-            var searchResultElement = ''
-            _.each(loadedData, function(searchResult) {
-                var text = searchResult['match_content']
-                var file = searchResult['file_path']
-                var line = searchResult['line_number']
-                var newRowElement = '<tr><td class="searchResult">' + text +
-                                    '</td><td class="filePath">' + file +
-                                    '</td><td class="lineNumber">' + line +
-                                    '</td><td class="actionButtons">' +
-                                        '<span class="getContext">🔎</span> ' +
-                                    '</td></tr>'
-                searchResultElement = searchResultElement + newRowElement
-            })
-            $('#searchResults').append(searchResultElement)
-            setResultActions('search')
-    });
-});
-*/
-
 function renderSearchResults(searchData) {
     // Set visibility of results containers
     $('#searchResults')[0].innerHTML = ''
@@ -216,52 +142,6 @@ function renderSearchResults(searchData) {
     $('#searchResults').append(searchResultElement)
     setResultActions('search')
 }
-
-/*
-function fetchQuestions(status) {
-    console.log( "Handler for search submit called." );
-    $('#searchContent')[0].style.display = 'none'
-    $('#todoContent')[0].style.display = 'none'
-    $('#questionContent')[0].style.display = 'block'
-
-    $('#questionList')[0].innerHTML = ''
-    var directoryFilter = $('#directoryFilter')[0].value
-
-    $.get('get_questions', {status: status, directory_filter: directoryFilter},
-        function(questionData){
-            var loadedData = JSON.parse(questionData)
-            var questionResultElement = ''
-            _.each(loadedData, function(questionResult) {
-                var text = questionResult['question']
-                var file = questionResult['file_path']
-                var line = questionResult['line_number']
-                var answer = questionResult['answer']
-                var newRowElement = '<tr><td class="questionText">' + text +
-                                    '</td><td class="filePath">' + file +
-                                    '</td><td class="lineNumber">' + line +
-                                    '</td><td>' + answer +
-                                    '</td><td class="actionButtons">' +
-                                        '<span class="getContext">🔎</span> ' +
-                                    '</td></tr>'
-                questionResultElement = questionResultElement + newRowElement
-            })
-            $('#questionList').append(questionResultElement)
-            setResultActions('question')
-    });
-};
-
-$("#fetchAll").click(function() {
-    fetchQuestions('all')
-});
-
-$("#fetchAnswered").click(function() {
-    fetchQuestions('answered')
-});
-
-$("#fetchUnanswered").click(function() {
-    fetchQuestions('unanswered')
-});
-*/
 
 function renderQuestionResults(questionData) {
     // Set visibility of results containers
