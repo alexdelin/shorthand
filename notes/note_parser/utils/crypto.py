078af46e44da201cda1b5c96886a761fb77615bf
Alex Delin <alexdelin@me.com>
2019-08-12 19:23:17 -0400

    finish implementation of crypto basics

--- notes/note_parser/utils/crypto.py
+++ notes/note_parser/utils/crypto.py
@@ -1,18 +1,8 @@
 '''
 A module for very simple and reasonably
 efficint encryption & decryption via AES
-
-This implementation is designed to produce deterministic but
-pseudo-random initialization vectors for use with AES-CBC
-encryption.
-    - While this is not widely considered best practice, the
-      property of having deterministic encryption is extremely
-      useful for tracking line-level file changes
-    - The security implications of this approach are acceptable
-      for this use case. Attackers will be able to tell that two
-      lines are identical (even across files) and will know the
-      hash of each line, but each of these weaknesses have
-      negligible impact on the overall security of the approach
+with hashes for quickly comparing between
+plaintext and encrypted content
 '''
 
 import base64
@@ -21,25 +11,25 @@ from Crypto import Random
 from Crypto.Cipher import AES
 
 
-class AESCipher(object):
+class CryptoHelper(object):
 
     def __init__(self, key):
         self.bs = 32
         self.key = hashlib.sha256(key.encode()).digest()
 
-    def encrypt(self, raw, iv):
+    def encrypt(self, raw):
         '''Encrypt a string, using an initialization vector
         which is specified manually
         '''
         raw = self._pad(raw)
-        # iv = Random.new().read(AES.block_size)
+        iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+        return base64.urlsafe_b64encode(iv + cipher.encrypt(raw))
 
     def decrypt(self, enc):
         '''Decrypt a string encrypted using the `encrypt()` method
         '''
-        enc = base64.b64decode(enc)
+        enc = base64.urlsafe_b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
@@ -56,15 +46,32 @@ class AESCipher(object):
         '''
         return s[:-ord(s[len(s)-1:])]
 
+    def hash_string(self, input_string, size=20):
 
-def encrypt_file_path(cipher, file_path):
-    '''Encrypts a file path deterministically such that
-    the folder structure is preserved
-    '''
-    pass
+        complete_hash = hashlib.sha256(input_string).hexdigest()
+        truncated_hash = complete_hash[-size:]
+        return truncated_hash
 
+    def mask_string(self, input_string):
+        '''Consumes a string to process, and returns a string that includes
+        both a truncated hash of the input contents and an encrypted copy of
+        the input contents
+        '''
 
-def encrypt_file_contents(cipher, file_path, file_contents):
-    '''Encrypts the
-    '''
-    pass
+        content_hash = self.hash_string(input_string)
+        encrypted_content = cipher.encrypt(input_string)
+        full_output = content_hash + '@' + encrypted_content
+        return full_output
+
+    def unmask_string(self, masked_string):
+        '''Consumes a masked string and decrypts the encrypted contents
+        and verifies the hash of the contents
+        '''
+        split_mask = masked_string.split('@')
+        content_hash = split_mask[0]
+        encrypted_content = split_mask[1]
+        decrypted_content = self.decrypt(encrypted_content)
+        decrypted_hash = self.hash_string(decrypted_content)
+        if decrypted_hash != content_hash:
+            raise ValueError('Computed hash does not match expected value')
+        return decrypted_content
