a23485ffacaf2fe991b1d99a989652e0ae4d7daa
Alex Delin <alexdelin@me.com>
2020-01-20 22:55:47 -0500

    add deployment resources for raspberry pi

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export APP_USER=$(whoami)
+export APP_USER='pi'
 export APP_GROUP='pi'
 export CONFIG_LOCATION=/etc/apps/shorthand/shorthand_config.json
 
@@ -9,6 +9,10 @@ sudo mkdir /etc/apps
 sudo chown ${APP_USER}:${APP_GROUP} /etc/apps
 mkdir /etc/apps/shorthand
 cp sample_config.json ${CONFIG_LOCATION}
+
+sudo mkdir -p /etc/uwsgi/sites
+sudo chown ${APP_USER}:${APP_GROUP} /etc/uwsgi/sites
+cp uwsgi/shorthand_uwsgi_conf.ini /etc/uwsgi/sites/shorthand_uwsgi_conf.ini
 
 # Make dirs for log files
 sudo mkdir /var/log/apps
@@ -20,6 +24,26 @@ sudo mkdir /var/run/apps
 sudo chown ${APP_USER}:${APP_GROUP} /var/run/apps
 mkdir /var/run/apps/shorthand
 
+# Create a new Virtualenv and install the shorthand package
 mkvirtualenv -p $(which python3.7) shorthand
 pip install -r requirements.txt
 python setup.py develop
+
+# Install Nginx
+sudo apt install nginx
+sudo cp nginx/shorthand.conf /etc/nginx/sites-available/shorthand
+sudo ln -s /etc/nginx/sites-available/shorthand /etc/nginx/sites-enabled/shorthand
+
+# Add the pi user to the www-data group
+sudo usermod -a -G www-data pi
+
+# Set up as a service
+sudo cp systemd/shorthand.service /etc/systemd/system/shorthand.service
+
+# Start / Restart services
+sudo systemctl start shorthand
+sudo systemctl restart nginx
+
+# Enable services to run on boot
+sudo systemctl enable shorthand
+sudo systemctl enable nginx
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,16 @@
 rm -rf /etc/apps/shorthand
 rm -rf /var/log/apps/shorthand
 rm -rf /var/run/apps/shorthand
+rm /tmp/shorthand.sock
+rm /etc/uwsgi/sites/shorthand_uwsgi_conf.ini
+rm /etc/nginx/sites-available/shorthand
+rm /etc/nginx/sites-enabled/shorthand
+
+sudo systemctl stop shorthand
+sudo systemctl disable shorthand
+sudo systemctl restart nginx
+
+rm /etc/systemd/system/shorthand.service
 
 deactivate
 rmvirtualenv shorthand
