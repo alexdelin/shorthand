5511253b4f4db3d3766588c01e0518e2042c8199
Alex Delin <alexdelin@me.com>
2020-02-18 18:10:02 -0500

    start on validations

--- shorthand/utils/rec.py
+++ shorthand/utils/rec.py
@@ -16,7 +16,128 @@ PRIMITIVE_TYPES = [
     "int", "line", "date", "bool", "real"]
 
 ALL_TYPES = [
-    "int", "line", "date", "bool", "real", "range", "enum"]
+    "int", "line", "date", "bool", "real",
+    "range", "enum", "size", "regexp"]
+
+
+class RecordSet(object):
+    """Record Set object which holds the field configuration
+    and full record contents for one type of records in a GNU
+    recfile
+    """
+
+    def __init__(self, config):
+        super(RecordSet, self).__init__()
+        if config:
+            if self.validate_config(config):
+                self.config = config
+            else:
+                raise ValueError('Got Invalid configuration')
+        else:
+            self.config = {}
+        self.fields = {}
+        self.records = []
+
+    def validate_config(self, config):
+        '''Validate configuration for a record set
+        '''
+        return True
+
+    def validate_record(self, record):
+        '''Validate an individual record against its defined
+        type and properties
+        '''
+
+        # Validate Mandatory Fields
+        for mandatory_field in self.config.get('mandatory', []):
+            if mandatory_field not in record.keys():
+                return f'Missing mandatory field {mandatory_field}'
+
+        # Validate Unique Fields
+        for unique_field in self.config.get('unique', []):
+            if len(record.get('unique', [])) > 1:
+                return f'More than 1 value found for unique field {unique_field}'
+
+        # Validate Allowed Fields
+        # Validate Prohibited Fields
+        # Validate Primary Key
+        # Validate Size Constraint
+        # Validate types of all specified fields
+            # Validate int field
+            # Validate line field
+            # Validate date field
+            # Validate bool field
+            # Validate real field
+            # Validate range field
+            # Validate enum field
+            # Validate size field
+            # Validate regexp field
+        # Auto-generate fields
+        # Add a new field to the field list
+        return False
+
+    def insert(self, records):
+        '''Insert raw dictionaries into the record set
+        Typically only called by internal methods
+        '''
+        for record in records:
+            error_message = self.validate_record(record)
+            if not error_message:
+                self.records.append(record)
+            else:
+                raise ValueError(f'Validation Error: {error_message} in record {record}')
+
+    def get_rec(self):
+        '''Serialize the record set to recfile format
+        '''
+        pass
+
+    def save_rec(self, file_path):
+        '''Write the contents of the record set to a file in recfile format
+        '''
+        pass
+
+    def insert_rec(self, records_string):
+        '''Insert one or more new records into the record set from a string
+        in recfile format
+        '''
+        pass
+
+    def get_csv(self):
+        '''Serialize the record set to a string of CSV data
+        '''
+        pass
+
+    def insert_csv(self, csv_data):
+        '''Import CSV data to update the record set
+        '''
+        pass
+
+    def get_json(self):
+        '''serialize the record set to a JSON string
+        '''
+        return json.dumps(self.records)
+
+    def insert_json(self, json_data):
+        '''Import JSON data to update the record set
+        '''
+        records = json.loads(json_data)
+        self.insert(records)
+
+    def get_fields(self):
+        '''Get all of the fields present in the record set
+        '''
+        return self.fields
+
+    def get_record_count(self):
+        '''Get the total number of records in the record set
+        '''
+        return len(self.records)
+
+    def all(self):
+        '''Get all records in the record set as a list of dictionaries
+        '''
+        return self.records
 
 
 def load_from_file(file_path):
@@ -70,8 +191,24 @@ def process_type_definition(definition_string):
             'values': no_comments.split(' ')
         }
 
+    elif linked_type_name == 'size':
+        if len(extra_params) != 1:
+            raise ValueError('Only a single size limit can be specified for a size type')
+        else:
+            return {
+                'type': 'size',
+                'limit': extra_params[0]
+            }
+
+    elif linked_type_name == 'regexp':
+        extra_params_string = ' '.join(extra_params).strip()
+        return {
+            'type': 'regexp',
+            'pattern': extra_params_string
+        }
+
     else:
-        raise valueError(f'Unknown type {linked_type_name} specified')
+        raise ValueError(f'Unknown type {linked_type_name} specified')
 
 
 def load_from_string(input_string):
@@ -184,7 +321,7 @@ def load_from_string(input_string):
                         raise ValueError(f'Undefined type {type_name} specified '
                                          f'for field {field_name}')
 
-    print('Got config ---')
+    print('--- Got config ---')
     print(json.dumps(record_set_config))
 
     record_set = RecordSet(config=record_set_config)
@@ -194,94 +331,38 @@ def load_from_string(input_string):
     for idx, line in enumerate(split_lines):
 
         if not line.strip():
+            # Empty line
             if current_record:
                 records.append(current_record)
                 current_record = {}
+                continue
+            else:
+                continue
 
         if re.match(r'[a-zA-Z][a-zA-Z0-9_]*: .*', line):
+            # A field value
             split_line = line.split(':', 1)
             key = split_line[0]
-            value = split_line[1]
+            value = split_line[1].lstrip(' ')
             current_record.setdefault(key, [])
             current_record[key].append(value)
-            
+            continue
+
         if line[0] == '%':
             # we have reached the start of a new record set
-            break
-            
-    for record in records:
-        record_set.insert(record)
-        
+            if current_record:
+                raise ValueError('Found the start of a new record set '
+                                 'in the middle of a record')
+            continue
+
+    record_set.insert(records)
+
+    print('--- Record Set JSON ---')
+    print(record_set.get_json())
+
     return record_set
 
 
 load_from_file('/Users/alexdelin/code/shorthand/sublime_plugins/test.rec')
 
 
-class RecordSet(object):
-    """Record Set object which holds the field configuration
-    and full record contents for one type of records in a GNU
-    recfile
-    """
-
-    def __init__(self, config):
-        super(RecordSet, self).__init__()
-        self.config = config
-        self.fields = {}
-        self.records = []
-        
-    def insert():
-        '''insert raw dictionaries into the record set
-        '''
-        pass
-
-    def get_rec():
-        '''Serialize the record set to recfile format
-        '''
-        pass
-
-    def save_rec(file_path):
-        '''Write the contents of the record set to a file in recfile format
-        '''
-        pass
-        
-    def insert_rec(records_string):
-        '''Insert one or more new records into the record set from a string
-        in recfile format
-        '''
-        pass
-
-    def get_csv():
-        '''Serialize the record set to a string of CSV data
-        '''
-        pass
-
-    def insert_csv(csv_data):
-        '''Import CSV data to update the record set
-        '''
-        pass
-        
-    def get_json():
-        '''serialize the record set to a JSON string
-        '''
-        pass
-        
-    def insert_json():
-        '''Import JSON data to update the record set
-        '''
-        pass
-
-    def get_fields():
-        '''Get all of the fields present in the record set
-        '''
-        return self.fields
-
-    def get_record_count():
-        '''Get the total number of records in the record set
-        '''
-        return len(self.records)
-
-    def all():
-        '''Get all records in the record set as a list
-        '''
-        return self.records
