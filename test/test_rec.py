5115e8c3f6d6f815cff507daf98164f77501cd4a
Alex Delin <alexdelin@me.com>
2020-02-21 09:08:38 -0500

    load sample rec configs

--- test/test_rec.py
+++ test/test_rec.py
@@ -1,5 +1,7 @@
 import unittest
+import re
 
+from shorthand.utils.rec import load_from_string
 from utils import setup_environment
 from model import NoteparserModel
 
@@ -17,7 +19,12 @@ class TestRec(unittest.TestCase):
         assert os.path.exists(test_dir)
 
     def test_valid_config_parsing(self):
-        pass
+        with open('rec_data/valid_config.rec', 'r') as f:
+        valid_config_data = f.read()
+        split_config = re.split(r'#.*?\n', valid_config_data)
+        
+        for valid_config in split_config:
+            config = load_from_string(valid_config)
 
     def test_invalid_config_parsing(self):
         pass
