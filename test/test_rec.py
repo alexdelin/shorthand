c4cc10f412d0a0a8b6ba7c354cde145c880dcd22
Alex Delin <alexdelin@me.com>
2020-02-21 17:27:42 -0500

    basic valid and invalid examples

--- test/test_rec.py
+++ test/test_rec.py
@@ -1,5 +1,8 @@
-import unittest
+import os
 import re
+import unittest
+
+import pytest
 
 from shorthand.utils.rec import load_from_string
 from utils import setup_environment
@@ -20,14 +23,24 @@ class TestRec(unittest.TestCase):
 
     def test_valid_config_parsing(self):
         with open('rec_data/valid_config.rec', 'r') as f:
-        valid_config_data = f.read()
-        split_config = re.split(r'#.*?\n', valid_config_data)
-        
+            valid_config_data = f.read()
+            split_config = re.split(r'#.*?\n', valid_config_data)
+            split_config = [config for config in split_config if config]
+
         for valid_config in split_config:
-            config = load_from_string(valid_config)
+            record_set = load_from_string(valid_config)
+            assert record_set.config.keys()
 
     def test_invalid_config_parsing(self):
-        pass
+        with open('rec_data/invalid_config.rec', 'r') as f:
+            invalid_config_data = f.read()
+        split_config = re.split(r'#.*?\n', invalid_config_data)
+        split_config = [config for config in split_config if config]
+
+        for invalid_config in split_config:
+            with pytest.raises(ValueError) as e:
+                record_set = load_from_string(invalid_config)
+            assert str(e.value)
 
     def test_valid_record_parsing(self):
         pass
