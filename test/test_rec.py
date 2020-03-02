f6e39535cd64529ce01038daf1f7409336fb439b
Alex Delin <alexdelin@me.com>
2020-03-01 19:03:28 -0700

    add wireframe for testing full record set parsing

--- test/test_rec.py
+++ test/test_rec.py
@@ -22,8 +22,8 @@ class TestRec(unittest.TestCase):
     def test_valid_config_parsing(self):
         with open('rec_data/valid_config.rec', 'r') as f:
             valid_config_data = f.read()
-            split_config = re.split(r'#.*?\n', valid_config_data)
-            split_config = [config for config in split_config if config]
+        split_config = re.split(r'#.*?\n', valid_config_data)
+        split_config = [config for config in split_config if config]
 
         for valid_config in split_config:
             record_set = load_from_string(valid_config)
@@ -41,7 +41,21 @@ class TestRec(unittest.TestCase):
             assert str(e.value)
 
     def test_valid_record_parsing(self):
-        pass
+        with open('rec_data/valid_records.rec', 'r') as f:
+            valid_record_data = f.read()
+        valid_record_sets = re.split(r'#.*?\n', valid_record_data)
+        valid_record_sets = [record_set for record_set in valid_record_sets if record_set]
+        for valid_record_set in valid_record_sets:
+            loaded_record_set = load_from_string(valid_record_set)
+            assert loaded_record_set.records
 
     def test_invalid_record_parsing(self):
-        pass
+        with open('rec_data/invalid_records.rec', 'r') as f:
+            invalid_record_data = f.read()
+        invalid_record_sets = re.split(r'#.*?\n', invalid_record_data)
+        invalid_record_sets = [record_set for record_set in invalid_record_sets if record_set]
+        for invalid_record_set in invalid_record_sets:
+            with pytest.raises(ValueError) as e:
+                loaded_record_set = load_from_string(invalid_record_set)
+            assert str(e.value)
+
