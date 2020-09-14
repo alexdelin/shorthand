440789f010b2f02365d9b407e56ed53ad3cbf7d2
Alex Delin <alexdelin@me.com>
2020-09-14 17:34:24 -0400

    PoC implementation of the file finder UI

--- web/js/shorthand-search.js
+++ web/js/shorthand-search.js
@@ -24,3 +24,65 @@ function renderSearchResults(searchData) {
     setCount(loadedData['count'])
     setResultActions('search')
 }
+
+function showFileFinder() {
+    $('#shorthandFileModal').modal();
+    var taTest = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/api/v1/files?query_string=%QUERY',
+            wildcard: '%QUERY'
+        }
+    });
+
+    $('#fileModalSearchBar').typeahead(null, {
+        name: 'notes-files',
+        highlight: true,
+        limit: 10,
+        source: taTest,
+    });
+
+    // Click Handler for the goto note button
+    $("#goToNote").click( function() {
+        console.log('Go To Note clicked!');
+        var notePath = $('#fileModalSearchBar').val();
+        // Log the file view
+        $.ajax({
+            url: '/api/v1/record_view?' + $.param({relative_path: notePath}),
+            type: 'POST',
+            success: function(responseData) {
+                if (responseData == 'ack') {
+                    window.location.href = '/render?path=' + notePath;
+                }
+            },
+            error: function(responseData) {
+                var loadedResponse = JSON.parse(responseData.responseText)
+                showModal(loadedResponse.error)
+            }
+        });
+    });
+
+    // Set the focus to the search bar after the modal is actually visible
+    $('#shorthandFileModal').on('shown.bs.modal', function (e) {
+        $('#fileModalSearchBar').focus();
+    })
+
+    // Handler for the enter key
+    // from https://stackoverflow.com/a/7060762
+    $("#fileModalSearchBar").on('keyup', function (e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            $('#goToNote').click();
+        }
+    });
+
+};
+
+// From https://stackoverflow.com/a/16006607
+function KeyPress(e) {
+    var evtobj = window.event? event : e
+    if (evtobj.keyCode == 84 && evtobj.ctrlKey) {
+        showFileFinder();
+    };
+}
+document.onkeydown = KeyPress;
