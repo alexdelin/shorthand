f5fdf5050b5fcf22e4aa785062ede60e70ec81f7
Alex Delin <alexdelin@me.com>
2021-01-05 15:18:43 -0700

    implement checking for invalid links in notes

--- shorthand/notes.py
+++ shorthand/notes.py
@@ -1,6 +1,13 @@
+import re
+import os
+from subprocess import Popen, PIPE
 import logging
 
-from shorthand.utils.paths import get_full_path
+from shorthand.utils.paths import get_full_path, get_relative_path
+from shorthand.utils.patterns import INTERNAL_LINK_PATTERN
+
+
+link_regex = re.compile(INTERNAL_LINK_PATTERN)
 
 
 log = logging.getLogger(__name__)
@@ -44,3 +51,58 @@ def _create_note():
     '''Create a new note
     '''
     raise NotImplementedError('Not implemented yet!')
+
+
+def _validate_internal_links(notes_directory, grep_path='grep'):
+    '''Validate that all of the internal links within notes point
+       to files that actually exist within the notes directory (not
+           necessarily other notes files)
+    '''
+
+    invalid_links = []
+
+    # Use Grep to find all internal links
+    grep_command = '{grep_path} -Prn "{pattern}" '\
+                   '--include="*.note" {dir}'.format(
+                        grep_path=grep_path,
+                        pattern=INTERNAL_LINK_PATTERN,
+                        dir=notes_directory)
+    log.debug(f'Running grep command {grep_command} to get internal links')
+
+    proc = Popen(
+        grep_command,
+        stdout=PIPE, stderr=PIPE,
+        shell=True)
+    output, err = proc.communicate()
+    output_lines = output.decode().split('\n')
+
+    for line in output_lines:
+
+        if not line.strip():
+            continue
+
+        split_line = line.split(':', 2)
+
+        file_path = split_line[0].strip()
+        line_number = split_line[1].strip()
+        match_content = split_line[2].strip()
+
+        note_path = get_relative_path(notes_directory, file_path)
+
+        matches = link_regex.findall(match_content)
+        for match in matches:
+            # The matching group for the text starts
+            # with `[` and ends with `](`
+            link_text = match[0][1:-2]
+            link_target = match[1]
+            link_full_target = get_full_path(notes_directory, link_target)
+            if not os.path.exists(link_full_target):
+                link = {
+                    'line_number': line_number,
+                    'path': note_path,
+                    'link_target': link_target,
+                    'link_text': link_text
+                }
+                invalid_links.append(link)
+
+    return invalid_links
