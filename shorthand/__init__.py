a0716b0772dc0b1b81a93c6509a07c39f734bb4b
Alex Delin <alexdelin@me.com>
2021-04-19 18:04:01 -0600

    add more methods to the server class

--- shorthand/__init__.py
+++ shorthand/__init__.py
@@ -1,9 +1,14 @@
 import logging
 
-from shorthand.notes import _get_note, _update_note
+from shorthand.notes import _get_note, _update_note, \
+                            _validate_internal_links, _append_to_note, \
+                            _create_note, _get_backlinks, _get_links
+from shorthand.calendar import _get_calendar
+from shorthand.tags import _get_tags
+from shorthand.stamping import _stamp_notes
 from shorthand.elements.todos import _get_todos, _mark_todo
 from shorthand.utils.config import get_notes_config, write_config, \
-    modify_config
+                                   modify_config
 from shorthand.utils.logging import get_handler
 
 
@@ -18,10 +23,10 @@ class ShorthandServer(object):
         '''Initialize the server with the configuration
            in the specified file
         '''
-        # super(ShorthandServer, self).__init__()
+        self.log = logging.getLogger(__name__)
+
         self.config_path = config_path
         self.reload_config()
-        self.log = logging.getLogger(__name__)
         log_handler = get_handler(self.config)
         self.log.addHandler(log_handler)
 
@@ -69,34 +74,63 @@ class ShorthandServer(object):
         return _update_note(notes_directory=self.config['notes_directory'],
                             file_path=note_path, content=content)
 
-    def append_to_note(self):
-        raise NotImplementedError
+    def append_to_note(self, note_path, content, blank_lines=1):
+        return _append_to_note(notes_directory=self.config['notes_directory'],
+                               note_path=note_path, content=content,
+                               blank_lines=blank_lines)
 
-    def create_note(self):
-        raise NotImplementedError
+    def create_note(self, note_path, content=None):
+        return _create_note(notes_directory=self.config['notes_directory'],
+                            note_path=note_path, content=content)
+
+    def validate_internal_links(self):
+        return _validate_internal_links(
+            notes_directory=self.config['notes_directory'],
+            grep_path=self.config['grep_path'])
+
+    def get_backlinks(self, note_path):
+        return _get_backlinks(notes_directory=self.config['notes_directory'],
+                              note_path=note_path,
+                              grep_path=self.config['grep_path'])
+
+    def get_links(self, source=None, target=None, include_external=False,
+                  include_invalid=False):
+        return _get_links(notes_directory=self.config['notes_directory'],
+                          source=source, target=target,
+                          include_external=include_external,
+                          include_invalid=include_invalid,
+                          grep_path=self.config['grep_path'])
 
     # Stamping
-    def stamp_notes(stamp_todos=True, stamp_today=True, stamp_questions=True,
-                    stamp_answers=True):
-        pass
+    def stamp_notes(self, stamp_todos=True, stamp_today=True,
+                    stamp_questions=True, stamp_answers=True):
+        return _stamp_notes(notes_directory=self.config['notes_directory'],
+                            stamp_todos=stamp_todos, stamp_today=stamp_today,
+                            stamp_questions=stamp_questions,
+                            stamp_answers=stamp_answers,
+                            grep_path=self.config['grep_path'])
 
     # Calendar
-    def get_calendar(directory_filter=None):
-        pass
+    def get_calendar(self, directory_filter=None):
+        return _get_calendar(notes_directory=self.config['notes_directory'],
+                             directory_filter=directory_filter,
+                             grep_path=self.config['grep_path'])
 
     # Tags
-    def get_tags(directory_filter=None):
-        pass
+    def get_tags(self, directory_filter=None):
+        return _get_tags(notes_directory=self.config['notes_directory'],
+                         directory_filter=directory_filter,
+                         grep_path=self.config['grep_path'])
 
     # TOC
-    def get_toc(directory_filter=None):
+    def get_toc(self, directory_filter=None):
         pass
 
     # Typeahead
-    def update_ngram_database():
+    def update_ngram_database(self):
         pass
 
-    def get_typeahead_suggestions(query_string, limit=10):
+    def get_typeahead_suggestions(self, query_string, limit=10):
         pass
 
     # ----------------
