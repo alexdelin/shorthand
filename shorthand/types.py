11403d8c376ccd2ca6a1424f32f2a5280265675c
Alex Delin <alexdelin@me.com>
2022-11-16 16:02:14 -0700

    finish typing search results in the backend

--- shorthand/types.py
+++ shorthand/types.py
@@ -10,15 +10,30 @@ ExecutablePath = str
 # An absolute path to a directory on the local filesystem (with no trailing slash)
 DirectoryPath = str
 
+
 # Notes directory level concepts
 # ------------------------------
 
 # An absolute path to a note from the root of the notes directory
 NotePath = str
 
+# A nicely formatted
+DisplayPath = str
+
 # A relative path to a note from another note.
 # Typically must be transformed into a NotePath before it can be used
 RelativeNotePath = str
+
+# A subdirectory within the notes directory
+Subdir = str
+
+
+# Note level concepts
+# -------------------
+
+# A URL which points to an external resource
+ExternalURL = str
+
 
 # API level concepts
 # ------------------
