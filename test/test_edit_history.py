ea7f2c08b97db7599c27bf05840cff8df083c553
Alex Delin <alexdelin@me.com>
2024-08-02 22:25:09 -0600

    clean up pytest setup

--- test/test_edit_history.py
+++ test/test_edit_history.py
@@ -18,6 +18,8 @@ class TestEditHistory(unittest.TestCase):
     def setup_class(cls):
         # ensure that we have a clean environment before running any tests
         cls.config = setup_environment()
+        cls.notes_dir = cls.config['notes_directory']
+        cls.find_path = cls.config['find_path']
 
     @classmethod
     def teardown_class(cls):
@@ -33,34 +35,34 @@ class TestEditHistory(unittest.TestCase):
 
     def test_storing_daily_note_version(self):
         for test_note in ['/section/mixed.note', '/bugs.note']:
-            ensure_daily_starting_version(notes_directory=self.config['notes_directory'],
+            ensure_daily_starting_version(notes_directory=self.notes_dir,
                                           note_path=test_note)
             current_utc_day = datetime.now(UTC).date().isoformat()
 
             # Check the version file was written
-            assert os.path.exists(f'{self.config["notes_directory"]}/{HISTORY_PATH}/{test_note}/{current_utc_day}.version')
+            assert os.path.exists(f'{self.notes_dir}/{HISTORY_PATH}/{test_note}/{current_utc_day}.version')
 
             # Check we can get the version via the API
             note_versions = _list_versions_for_note(
-                notes_directory=self.config['notes_directory'],
+                notes_directory=self.notes_dir,
                 note_path=test_note,
-                find_path=self.config['find_path'])
+                find_path=self.find_path)
             assert note_versions == [current_utc_day]
 
     def test_storing_edit_diffs(self):
         _store_history_for_note_edit(
-            notes_directory=self.config['notes_directory'],
+            notes_directory=self.notes_dir,
             note_path='/todos.note',
             new_content='foo bar')
 
         note_versions = _list_versions_for_note(
-            notes_directory=self.config['notes_directory'],
+            notes_directory=self.notes_dir,
             note_path='/todos.note',
-            find_path=self.config['find_path'])
+            find_path=self.find_path)
         assert len(note_versions) == 1
 
         note_diffs = _list_diffs_for_note(
-            notes_directory=self.config['notes_directory'],
+            notes_directory=self.notes_dir,
             note_path='/todos.note',
-            find_path=self.config['find_path'])
+            find_path=self.find_path)
         assert len(note_diffs) == 1
