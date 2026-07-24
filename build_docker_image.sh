22fa78a59a130e4eda94845a7f5e285375ea7fa5
Alex Delin <alexdelin@me.com>
2026-07-24 16:37:41 -0600

    bump version numbers

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export VERSION="0.1.2"
+export VERSION="0.2.0"
 
 cd react-frontend/
 npm run build
@@ -10,6 +10,6 @@ cd ..
 
 docker build \
     --tag alexdelin/shorthand:${VERSION} \
-    --platform linux/arm/v7 \
+    --platform linux/arm64 \
     --push \
     .
